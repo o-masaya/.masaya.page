56d83bbebc09439d74bd7ade3ad0e1293beccc9b
o-masaya <142707056+o-masaya@users.noreply.github.com>
2023-08-24 11:26:56 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+# .masaya.page
+$(function(){
+    $('.hamburger').on('click',function(event,isInView,visiblePartX,visiblePartY){
+    hamburger();
+     });
+     $('#navi a').on('click',function(event,isInView,visiblePartX,visiblePartY){
+    hamburger();
+     });
+    });
+    function hamburger(){
+        $('.hamburger').toggleClass('active');
+        if($('.hamburger').hasClass('active')){
+            $('#navi').addClass('active');
+        }else{
+            $('#navi').removeClass('active');
+        }
+    }
